aedcc4151bcf2d2ee3dc22d5e0b34d2a87ae0f98
HughBGrant <112141259+HughBGrant@users.noreply.github.com>
2025-09-03 17:34:55 +0900

    update

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -9,11 +9,27 @@ public class Item : MonoBehaviour
     [SerializeField]
     int value;
     public int Value { get => value; }
-
     private float rotationSpeed = 20f;
 
+    private Rigidbody rb;
+    private SphereCollider sphereCollider;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        sphereCollider = rb.GetComponent<SphereCollider>();
+    }
     void Update()
     {
         transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            rb.isKinematic = true;
+            sphereCollider.enabled = false;
+        }
     }
 }
