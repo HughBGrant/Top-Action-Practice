484239e9e52ea429fab60fbfc2f70265cf393db5
HughBGrant <112141259+HughBGrant@users.noreply.github.com>
2025-09-05 19:48:20 +0900

    update

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth;
+
+    [SerializeField]
+    private int currentHealth;
+}
